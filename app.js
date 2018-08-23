385ce12753e132686a2328c7d655f2ca51f3f3cc
Ángel Guerrero <lasantiagoguerrero@gmail.com>
2018-08-23 11:56:59 -0500

    Corrigiendo identación a 2 espacios

--- app.js
+++ app.js
@@ -9,145 +9,143 @@ const markdownLinkExtractor = require('markdown-link-extractor');
  * @param {string} path Absolute file path for extract links
  */
 var extractLinks = function (path) {
-	const markdown = fs.readFileSync(path).toString();
+  const markdown = fs.readFileSync(path).toString();
 
-	return markdownLinkExtractor(markdown);
+  return markdownLinkExtractor(markdown);
 };
 
 function walk(dir) {
-	var results = [];
-	var list = fs.readdirSync(dir);
-	
-	list.forEach(function (file) {
-		// console.log(file);
-		
-		var newdir = dir + '/' + file;
-		var stat = fs.statSync(newdir);
+  var results = [];
+  var list = fs.readdirSync(dir);
 
-		// console.log(newdir);
+  list.forEach(function (file) {
+    // console.log(file);
 
-		// Check if is a subdirectory
-		if (stat && stat.isDirectory()) {
-			/* Recurse into a subdirectory */
-			results = results.concat(walk(newdir));
-		}
-		else {
-			/* Is a file */
-			results.push(`${newdir}`);
-		}
-	});
-	return results;
+    var newdir = dir + '/' + file;
+    var stat = fs.statSync(newdir);
+
+    // console.log(newdir);
+
+    // Check if is a subdirectory
+    if (stat && stat.isDirectory()) {
+      /* Recurse into a subdirectory */
+      results = results.concat(walk(newdir));
+    } else {
+      /* Is a file */
+      results.push(`${newdir}`);
+    }
+  });
+  return results;
 }
 
 var main = function (path, options) {
-	// Store the links
-	let linkArray = [];
+  // Store the links
+  let linkArray = [];
 
-	// Store the markdown files
-	let filesArray = [];
+  // Store the markdown files
+  let filesArray = [];
 
-	return new Promise((resolve, reject) => {
-		path = fs.realpathSync(path);
-		console.log(`Path: ${path}`);
+  return new Promise((resolve, reject) => {
+    path = fs.realpathSync(path);
+    console.log(`Path: ${path}`);
 
-		const stats = fs.statSync(path);
+    const stats = fs.statSync(path);
 
-		if (stats.isDirectory()) {
-			console.log("Es un directorio\n");
+    if (stats.isDirectory()) {
+      console.log("Es un directorio\n");
 
-			// Get the absolute path of files
-			filesArray = walk(path);
-			
-			// Count for exit the cicle
-			let counter = 0;
+      // Get the absolute path of files
+      filesArray = walk(path);
 
-			// Count of links
-			let countLinks = 0;
-			
-			filesArray.forEach((file) => {
-				console.log(`Extrayendo enlaces del directorio: ${file}`);
-				let extractedLinks = extractLinks(file);
-				countLinks += extractedLinks.length;
+      // Count for exit the cicle
+      let counter = 0;
 
-				console.log("Enlaces del archivo: ");
-				extractedLinks.forEach((link) => {
-					console.log(`- ${link}`);
+      // Count of links
+      let countLinks = 0;
 
-					linkCheck(link, (err, result) => {
+      filesArray.forEach((file) => {
+        console.log(`Extrayendo enlaces del directorio: ${file}`);
+        let extractedLinks = extractLinks(file);
+        countLinks += extractedLinks.length;
 
-						// Exit of function linkCheck if error occurs
-						if (err) return reject(err);
+        console.log("Enlaces del archivo: ");
+        extractedLinks.forEach((link) => {
+          console.log(`- ${link}`);
 
-						if (options.validate) {
-							linkArray.push({
-								file: file,
-								href: result.link,
-								// text: result.text, //Undefined
-								status: result.statusCode,
-								ok: result.status === 'alive'
-							});
-							counter++;
-						}
-						else {
-							linkArray.push({
-								file: file,
-								href: result.link,
-								// text: result.text, // undefined
-							});
-							counter++;
-						}
+          linkCheck(link, (err, result) => {
 
-						// The promise is fulfilled  
-						if (counter === countLinks) resolve(linkArray);
-					});
-				});
-				console.log("\n");
-			});
+            // Exit of function linkCheck if error occurs
+            if (err) return reject(err);
 
-		} else {
-			console.log(`Es sólo un archivo ${path}`);
+            if (options.validate) {
+              linkArray.push({
+                file: file,
+                href: result.link,
+                // text: result.text, //Undefined
+                status: result.statusCode,
+                ok: result.status === 'alive'
+              });
+              counter++;
+            } else {
+              linkArray.push({
+                file: file,
+                href: result.link,
+                // text: result.text, // undefined
+              });
+              counter++;
+            }
 
-			const extactedLinks = extractLinks(path);
+            // The promise is fulfilled  
+            if (counter === countLinks) resolve(linkArray);
+          });
+        });
+        console.log("\n");
+      });
 
-			extactedLinks.forEach(link => {
-				console.log(`Evaluando enlace: ${link}`);
+    } else {
+      console.log(`Es sólo un archivo ${path}`);
 
-				linkCheck(link, (err, result) => {
+      const extactedLinks = extractLinks(path);
 
-					// Exit of function linkCheck if error occurs
-					if (err) return reject(err);
+      extactedLinks.forEach(link => {
+        console.log(`Evaluando enlace: ${link}`);
 
-					if (options.validate)
-						linkArray.push({
-							file: path,
-							href: result.link,
-							// text: result.text, //Undefined
-							status: result.statusCode,
-							ok: result.status === 'alive'
-						});
-					else
-						linkArray.push({
-							file: path,
-							href: result.link,
-							// text: result.text, // undefined
-						});
+        linkCheck(link, (err, result) => {
 
-					// The promise is fulfilled  
-					if (linkArray.length === extactedLinks.length) resolve(linkArray);
-				});
+          // Exit of function linkCheck if error occurs
+          if (err) return reject(err);
 
-			});
+          if (options.validate)
+            linkArray.push({
+              file: path,
+              href: result.link,
+              // text: result.text, //Undefined
+              status: result.statusCode,
+              ok: result.status === 'alive'
+            });
+          else
+            linkArray.push({
+              file: path,
+              href: result.link,
+              // text: result.text, // undefined
+            });
 
-		}
+          // The promise is fulfilled  
+          if (linkArray.length === extactedLinks.length) resolve(linkArray);
+        });
 
-	});
+      });
+
+    }
+
+  });
 
 };
 
 
 
 main('public/', {
-		validate: true
-	})
-	.then((links) => console.log(links))
-	.catch((reason) => console.error(`Ha ocurrido un error: ${reason}`));
+    validate: true
+  })
+  .then((links) => console.log(links))
+  .catch((reason) => console.error(`Ha ocurrido un error: ${reason}`));
